69801e412981d8cb537899c77ed27c07b1f276f2
icanq <icanq@.localdomain>
2020-12-11 06:56:02 +0700

    adding navbar create logo fix game component add routes

---
+++ src/components/Navbar.vue
@@ -0,0 +1,39 @@
+<template>
+  <nav class="navbar" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <a class="navbar-item">
+        <img src="https://i.gifer.com/1AWw.gif">
+      </a>
+      <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
+    </div>
+    <div class="navbar-end">
+      <div class="navbar-item">
+        <div class="buttons">
+          <a class="button is-primary">
+            <router-link to="/">Home</router-link>
+          </a>
+          <a class="button is-light">
+            <router-link to="/game">Game</router-link>
+          </a>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Navbar'
+}
+</script>
+
+<style scoped>
+.button {
+  text-decoration: none;
+  color: white;
+}
+</style>
