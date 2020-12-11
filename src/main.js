9a770aaf9c26d6d07ce2363aa9a516946027123a
FebrianAditya <febrian.aditya@student.upi.edu>
2020-12-11 15:00:57 +0700

    final

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import VueSocketIO from 'vue-socket.io'
 
 Vue.use(new VueSocketIO({
   debug: false,
-  connection: 'http://localhost:3000',
+  connection: 'https://ghost-hunters.herokuapp.com',
   vuex: {
     store,
     actionPrefix: 'SOCKET_',
